f2a5a0c140fa974c2ed77b4b27ce0300b03f30a2
José Lucas <joselucasa937@gmail.com>
2024-02-27 18:56:22 -0300

    criado a navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800"],
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }

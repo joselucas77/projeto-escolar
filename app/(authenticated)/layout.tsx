b22e57ce01966b68c5909d2c5a0aafed61f4afe6
José Lucas <joselucasa937@gmail.com>
2024-02-27 19:03:53 -0300

    atualizei pastas e arquivos

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar";
+import Navbar from "../components/navbar";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800"],

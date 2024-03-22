5204b096ba1ddca30b3ddcd7c6d86ebcb68377b0
José Lucas <joselucasa937@gmail.com>
2024-03-22 11:07:49 -0300

    update

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
-import Navbar from "../components/navbar";
-import { AppProvider } from "../api/utils/context";
-import { Providers } from "./providers";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import "./style.css";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800"],
@@ -14,26 +9,18 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "SIE",
+  title: "Login",
   description: "Sistema Inovador Escolar",
 };
 
-export default function RootLayout({
+export default function LoginLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="pt-br" suppressHydrationWarning>
-      <body className={poppins.className}>
-        <AppProvider>
-          <Providers>
-            <Navbar />
-            {children}
-            <ToastContainer />
-          </Providers>
-        </AppProvider>
-      </body>
+    <html lang="pt-BR" className={poppins.className}>
+      <body>{children}</body>
     </html>
   );
 }
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,19 +1,37 @@
 "use client";
-import { useAppContext } from "../api/utils/context";
+import { useState } from "react";
+import Login from "../../components/(auth)/login";
+import Panel from "../../components/(auth)/panel";
+import Register from "../../components/(auth)/register";
 
-export default function Home() {
-  const { navbarActive } = useAppContext();
+const Auth = () => {
+  const [addClass, setAddClass] = useState(false);
+
+  const changeMode = () => {
+    setAddClass(!addClass);
+  };
+
   return (
     <div
-      className={`absolute h-screen px-3 py-3 bg-gray-300 dark:bg-gray-900 duration-500 ${
-        navbarActive
-          ? "w-[calc(100%-240px)] left-60"
-          : "w-[calc(100%-80px)] left-20"
+      className={`relative w-full min-h-screen overflow-hidden bg-white before:absolute before:w-[2000px] before:h-[2000px] before:rounded-[50%] before:bg-gradient-to-tl before:from-blue before:to-[#60a5fa] before:top-[-10%] before:right-[48%] before:-translate-y-2/4 before:z-[6] before:duration-[1.8s] before:ease-in-out ${
+        addClass
+          ? "before:translate-x-[100%] before:-translate-y-[50%] before:right-[52%]"
+          : ""
       }`}
     >
-      <main className="bg-white relative w-full h-full rounded-lg p-5 grid grid-cols-2 gap-x-3 text-gray-900 dark:text-white dark:bg-gray-800">
-        Home
-      </main>
+      <div className="absolute top-0 left-0 w-full h-full">
+        <div
+          className={`absolute z-[5] grid w-1/2 grid-cols-1s top-1/2 -translate-x-2/4 -translate-y-2/4 duration-[1s] delay-[.7s] ease-in-out ${
+            addClass ? "left-1/4" : "left-3/4"
+          }`}
+        >
+          <Login addClass={addClass} />
+          <Register addClass={addClass} />
+        </div>
+      </div>
+      <Panel changeMode={changeMode} addClass={addClass} />
     </div>
   );
-}
+};
+
+export default Auth;

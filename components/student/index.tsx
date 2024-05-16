a052e729dc4441652a89fcd006b48b3aa991db76
José Lucas <joselucasa937@gmail.com>
2024-05-16 19:04:51 -0300

    update studant page

--- components/student/index.tsx
+++ components/student/index.tsx
@@ -27,21 +27,6 @@ const Index = ({ params }: StudantProps) => {
             className="text-gray-900 bg-gray-300 hover:bg-gray-900 hover:text-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:text-gray-100 dark:hover:text-gray-900 dark:bg-gray-900 dark:hover:bg-gray-100 dark:focus:ring-blue-800"
           >
             <IoArrowUndo />
-            {/* <svg
-              className="w-4 h-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg> */}
           </button>
         </Link>
         <ProfileCard
